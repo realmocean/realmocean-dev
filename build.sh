e1f54059b941f994e035c225d4b313a0b8487ef0
selimtan <stanoncloud@gmail.com>
2023-12-22 14:38:23 +0300

    .

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build --no-cache --tag realmocean/install:latest .
+# docker build --no-cache --tag realmocean/install:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -t realmocean/install:latest --push .
